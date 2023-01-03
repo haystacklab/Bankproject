8569ff7c0b5d8c1f9479dc5eb6c963830ffa82cf
avinash <avinash.batlanki@gmail.com>
2023-01-03 14:03:47 -0600

    Added history endpoint

--- account-service/src/main/java/com/bankproject/accountservice/util/FeignServiceUtil.java
+++ account-service/src/main/java/com/bankproject/accountservice/util/FeignServiceUtil.java
@@ -1,7 +1,9 @@
 package com.bankproject.accountservice.util;
 
+import com.bank.domain.AccountType;
 import com.bank.domain.checking.CheckingAccount;
 import com.bank.domain.credit.CreditAccount;
+import com.bank.domain.history.Transaction;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,6 @@ public interface FeignServiceUtil {
     @GetMapping("/credit/get/{userId}")
     List<CreditAccount> getCreditAccounts(@PathVariable String userId);
 
-    @GetMapping("/history/get")
-    String getHistory();
+    @GetMapping("/history/get/{accountId}/{accountType}")
+    List<Transaction> getHistory(@PathVariable String accountId, @PathVariable AccountType accountType);
 }
