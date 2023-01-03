bfceb83dd60c889c168f61bfa269ebb661a78845
avinash <avinash.batlanki@gmail.com>
2023-01-03 14:07:54 -0600

    Implemented Builder pattern

--- domain/src/main/java/com/bank/domain/history/History.java
+++ domain/src/main/java/com/bank/domain/history/History.java
@@ -1,12 +1,44 @@
 package com.bank.domain.history;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
+@Getter
 public class History {
-    private HistoryType historyType;
-    private int periodInDays;
-    private List<Transaction> transactions;
+    private final int periodInDays;
+    private final List<Transaction> transactions;
+
+    public History(Builder builder) {
+        this.periodInDays = builder.periodInDays;
+        this.transactions = builder.transactions;
+    }
+
+    public static class Builder {
+        private int periodInDays;
+        private List<Transaction> transactions;
+
+        public static Builder newInstance() {
+            return new Builder();
+        }
+
+        private Builder() {}
+
+        public Builder setPeriodInDays(int periodInDays) {
+            this.periodInDays = periodInDays;
+            return this;
+        }
+
+        public Builder setTransactions(List<Transaction> transactions) {
+            this.transactions = transactions;
+            return this;
+        }
+
+        public History build() {
+            return new History(this);
+        }
+    }
 }
